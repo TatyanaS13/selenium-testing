8a824b075b7a4b3e13eb7bf891e54e2c8e9aad2c
Tatyana Smulskaya <tatyana.wrk@gmail.com>
2017-01-28 23:58:38 +0200

    change

--- example-test/test_example.py
+++ example-test/test_example.py
@@ -2,11 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome("/Users/tatyanas/Documents/chromedriver")
+    print(os.environ)
+    wd = webdriver.Chrome()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
@@ -16,4 +18,5 @@ def test_example(driver):
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver")
     driver.find_element_by_name("btnG").click()
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Пошук Google"))
+    WebDriverWait(driver, 20).until(EC.title_is("webdriver - Пошук Google"))
+
--- example-test/test_login.py
+++ example-test/test_login.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome("/Users/tatyanas/Documents/chromedriver")
+    wd = webdriver.Chrome()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
